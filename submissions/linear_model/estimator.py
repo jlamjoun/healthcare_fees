7d510df578cae145c849a32cb8e15910154c44c2
Jihane Lamjoun <jihanelamjoun@MacBook-Pro-de-Jihane.local>
2024-03-01 17:15:41 +0100

    Updated requirements/environment and fixed comments in estimator.py

--- submissions/linear_model/estimator.py
+++ submissions/linear_model/estimator.py
@@ -9,7 +9,7 @@ from sklearn.linear_model import LinearRegression
 def get_estimator():
     # Colonnes catégorielles et numériques
     categorical_cols = ["annee", "profession_sante", "region", "departement"]
-    numerical_cols = []  # Il n'y a pas de colonnes numériques dans vos données
+    numerical_cols = []  # Il n'y a pas de colonnes numériques dans nos données
 
     # Création du préprocesseur pour transformer les colonnes
     preprocessor = make_column_transformer(
